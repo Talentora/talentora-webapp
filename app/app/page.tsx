26212991242604b0aeb63a3c04679063a75cae17
Ben Gardiner <bengardiner18@gmail.com>
2024-08-09 19:32:52 -0400

    making new branch

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,14 @@
+import { VoiceClient } from "realtime-ai";
+
 const page = () => {
+
+
+    // const voiceClient = new VoiceClient({
+    //     baseUrl: "http://localhost:7860", // as an example
+    //     enableMic: true,
+        
+    // });
+    
     return (
         <div>
         <h1>Page</h1>
