f893a294414766922674ed7677d11bd4f529034c
Ben Gardiner <bengardiner18@gmail.com>
2024-08-09 19:26:33 -0400

    messing around with docker

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,43 +1,21 @@
-import CustomerPortalForm from '@/components/ui/AccountForms/CustomerPortalForm';
-import EmailForm from '@/components/ui/AccountForms/EmailForm';
-import NameForm from '@/components/ui/AccountForms/NameForm';
-import { redirect } from 'next/navigation';
-import { createClient } from '@/utils/supabase/server';
-import {
-  getUserDetails,
-  getSubscription,
-  getUser
-} from '@/utils/supabase/queries';
+import Link from 'next/link';
+import React from 'react';
 
-export default async function Account() {
-  const supabase = createClient();
-  const [user, userDetails, subscription] = await Promise.all([
-    getUser(supabase),
-    getUserDetails(supabase),
-    getSubscription(supabase)
-  ]);
-
-  if (!user) {
-    return redirect('/signin');
-  }
-
-  return (
-    <section className="mb-32 bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
-        <div className="sm:align-center sm:flex sm:flex-col">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Account
-          </h1>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            We partnered with Stripe for a simplified billing.
-          </p>
+const DashboardPage: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Account</h1>
+            <div className='flex flex-col'>
+                <Link href="/settings" className="text-blue-500 hover:underline">
+                    Settings
+                </Link>
+                <Link href="/app" className="text-blue-500 hover:underline">
+                    App
+                </Link>
+            </div>
+            
         </div>
-      </div>
-      <div className="p-4">
-        <CustomerPortalForm subscription={subscription} />
-        <NameForm userName={userDetails?.full_name ?? ''} />
-        <EmailForm userEmail={user.email} />
-      </div>
-    </section>
-  );
-}
+    );
+};
+
+export default DashboardPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
-import Pricing from '@/components/ui/Pricing/Pricing';
-import { createClient } from '@/utils/supabase/server';
-import {
-  getProducts,
-  getSubscription,
-  getUser
-} from '@/utils/supabase/queries';
+// import Pricing from '@/components/ui/Pricing/Pricing';
+import LandingPage from '@/components/ui/LandingPage/LandingPage';
+// import { createClient } from '@/utils/supabase/server';
+// import {
+//   getProducts,
+//   getSubscription,
+//   getUser
+// } from '@/utils/supabase/queries';
 
 export default async function PricingPage() {
-  const supabase = createClient();
-  const [user, products, subscription] = await Promise.all([
-    getUser(supabase),
-    getProducts(supabase),
-    getSubscription(supabase)
-  ]);
+  // const supabase = createClient();
+  // const [user, products, subscription] = await Promise.all([
+  //   getUser(supabase),
+  //   getProducts(supabase),
+  //   getSubscription(supabase)
+  // ]);
 
   return (
-    <Pricing
-      user={user}
-      products={products ?? []}
-      subscription={subscription}
-    />
+    // <Pricing
+    //   user={user}
+    //   products={products ?? []}
+    //   subscription={subscription}
+    // />
+    <LandingPage />
   );
 }
