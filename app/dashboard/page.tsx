263e12e5deb37fec27ee7162f7b5f98b3704365f
Ben Gardiner <bengardiner18@gmail.com>
2024-08-11 14:04:06 -0400

    i hate webstorm

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { ApplicantDashboard } from "@/components/applicant-dashboard";
+
+
+const page = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard</p>
+      <ApplicantDashboard />
+    </div>
+  );
+}
+
+export default page;
