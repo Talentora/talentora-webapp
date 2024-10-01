5f88e3a31cef58f35d683af3d7c461613cd05485
Lucas <junsuny789@gmail.com>
2024-09-30 20:34:20 -0400

    schema set

--- supabase/migrations/20240920165530_remote_schema.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."companies" add column "description" text;
-
-alter table "public"."jobs" add column "qualifications" text;
-
-alter table "public"."jobs" add column "requirements" text;
-
-
