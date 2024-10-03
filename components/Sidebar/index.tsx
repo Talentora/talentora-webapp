09cfdd012d31b8788b6310f6427a0a39ab8abd0f
Ben Gardiner <bengardiner18@gmail.com>
2024-10-03 16:38:21 -0400

    fixed href settings

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -50,7 +50,7 @@ const Page = () => {
         </Link>
         <Link
           className="flex items-center space-x-2 text-gray-700 hover:bg-gray-200 px-4 py-2 rounded"
-          href="dashboard/settings"
+          href="/settings"
         >
           <SettingsIcon className="h-5 w-5" />
           <span>Settings</span>
