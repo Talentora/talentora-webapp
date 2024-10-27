9364cf8d2ff14e20d8b5a14fadc582ba931ee2c0
Lucas <junsuny789@gmail.com>
2024-10-27 19:51:53 -0400

    up to date

--- supabase/migrations/20241016143737_remote_schema.sql
+++
@@ -1,5 +0,0 @@
-drop trigger if exists "on_auth_user_created" on "auth"."users";
-
-CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
-
-
