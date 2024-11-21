c020e05a38a9d4ac647c4bdb33d9f939941891b8
Ben Gardiner <bengardiner18@gmail.com>
2024-11-21 15:04:04 -0500

    finished joblist page

--- components/Jobs/job-row.tsx
+++ components/Jobs/job-row.tsx
@@ -3,12 +3,15 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { EnrichedJob } from './JobList';
 import { Badge } from '@/components/ui/badge';
+import { useRouter } from 'next/navigation';
 
 export function JobRow({ job }: { job: EnrichedJob }) {
+  const router = useRouter();
   return (
-    <TableRow>
-      <TableCell>{job.name}</TableCell>
-      <TableCell>
+    <TableRow onClick={() => router.push(`/jobs/${job.id}`)}>
+      <TableCell className="text-center">{job.id.slice(0, 6)}...</TableCell>
+      <TableCell className="text-center">{job.name}</TableCell>
+      <TableCell className="text-center">
         <Badge variant={job.status.toLowerCase() === 'open' ? 'success' : 'secondary'}>
           {job.status
             ? job.status.charAt(0).toUpperCase() +
@@ -16,42 +19,47 @@ export function JobRow({ job }: { job: EnrichedJob }) {
             : ''}
         </Badge>
       </TableCell>
-      <TableCell>
+      <TableCell className="text-center">
         {new Date(job.created_at).toLocaleDateString('en-US', {
           year: 'numeric',
           month: 'short',
           day: 'numeric'
         })}
       </TableCell>
-      <TableCell>
+      <TableCell className="text-center">
         <Badge variant={job.isConfigured ? 'success' : 'failure'}>
           {job.isConfigured ? 'Yes' : 'No'}
         </Badge>
       </TableCell>
-      <TableCell>
-        {job.departments?.length
-          ? job.departments
+      <TableCell className="text-center">
+        {job.departments?.length ? (
+          <div className="flex gap-1 flex-wrap justify-center">
+            {job.departments
               .filter(Boolean)
-              .map(dept => dept.name)
-              .join(', ')
-          : 'No department'}
+              .map(dept => (
+                <Badge key={dept.name} variant="secondary">
+                  {dept.name}
+                </Badge>
+              ))}
+          </div>
+        ) : (
+          'No department'
+        )}
       </TableCell>
-      <TableCell>
-        {job.offices?.length
-          ? job.offices
+      <TableCell className="text-center">
+        {job.offices?.length ? (
+          <div className="flex gap-1 flex-wrap justify-center">
+            {job.offices
               .filter(Boolean)
-              .map(office => office.name)
-              .join(', ')
-          : 'No office'}
-      </TableCell>
-
-
-     
-     
-      <TableCell>
-        <Link href={`/jobs/${job.id}`} passHref>
-          <Button variant="outline">View details</Button>
-        </Link>
+              .map(office => (
+                <Badge key={office.name} variant="secondary">
+                  {office.name}
+                </Badge>
+              ))}
+          </div>
+        ) : (
+          'No office'
+        )}
       </TableCell>
     </TableRow>
   );
