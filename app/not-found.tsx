88e92ad726f8ff96ece5478a5a82cb55f34524b2
Ben Gardiner <bengardiner18@gmail.com>
2024-11-24 12:27:56 -0500

    ui changes to bot library

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 
 export default function Custom404() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground">
+    <div className="flex flex-col items-center justify-center min-h-screen text-foreground">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
       <p className="text-xl mb-8 text-muted-foreground">
         Oops! The page you&apos;re looking for doesn&apos;t exist.
