1e372c601445228886ff14b1f902b8de338d55ff
Ben Gardiner <bengardiner18@gmail.com>
2024-11-29 01:52:37 -0500

    bot can hear my responses

--- components/Bot/VideoInterviewSession/index.tsx
+++ components/Bot/VideoInterviewSession/index.tsx
@@ -75,7 +75,7 @@ export default function VideoInterviewSession({
         
       </div>
 
-      <main className="flex basis-1/2 gap-4 p-4 m-5">
+      <main className="flex basis-1/3 gap-4 p-4 m-5">
         {/* Sidebar */}
         <div className="flex-col h-full w-1/3 gap-2">
           <div className="flex-1">
