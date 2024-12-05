06bf0fed793c54f1e444281f93c72171ef243e8d
ojnim <seungmincho03@gmail.com>
2024-12-04 23:36:28 -0500

    radar chart update

--- components/ui/line-chart.tsx
+++ components/ui/line-chart.tsx
@@ -1,56 +1,56 @@
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// } from 'chart.js';
-// import { Line } from 'react-chartjs-2';
+ import {
+   Chart as ChartJS,
+   CategoryScale,
+   LinearScale,
+   PointElement,
+   LineElement,
+   Title,
+  Tooltip,
+   Legend
+ } from 'chart.js';
+ import { Line } from 'react-chartjs-2';
 
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+ Title,
+ Tooltip,
+ Legend
+ );
 
-// interface Dataset {
-//   label: string;
-//   data: number[];
-//   borderColor: string;
-//   backgroundColor: string;
-// }
+interface Dataset {
+  label: string;
+   data: number[];
+  borderColor: string;
+ backgroundColor: string;
+ }
 
-// interface LineChartProps {
-//   labels: string[];
-//   datasets: Dataset[];
-//   title?: string;
-// }
+ interface LineChartProps {
+   labels: string[];
+  datasets: Dataset[];
+  title?: string;
+}
 
-// export default function LineChart({ labels, datasets, title }: LineChartProps) {
-//   const options = {
-//     responsive: true,
-//     plugins: {
-//       legend: {
-//         position: 'top' as const
-//       },
-//       title: {
-//         display: !!title,
-//         text: title
-//       }
-//     }
-//   };
+export default function LineChart({ labels, datasets, title }: LineChartProps) {
+   const options = {
+    responsive: true,
+   plugins: {
+      legend: {
+        position: 'top' as const
+      },
+      title: {
+       display: !!title,
+         text: title
+      }
+     }
+   };
 
-//   const data = {
-//     labels,
-//     datasets
-//   };
+   const data = {
+    labels,
+     datasets
+   };
 
-//   return <Line options={options} data={data} />;
-// }
+  return <Line options={options} data={data} />;
+ }
