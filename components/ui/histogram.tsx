9d4e4add9b8752385587a25cd6f7a113a5164bf6
Lucas <junsuny789@gmail.com>
2024-12-09 18:37:11 -0500

    fix build

--- components/ui/histogram.tsx
+++ components/ui/histogram.tsx
@@ -1,73 +1,75 @@
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  } from 'chart.js';
-  import { Bar } from 'react-chartjs-2';
+// import {
+//     Chart as ChartJS,
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+//   } from 'chart.js';
+
+//   import { Bar } from 'react-chartjs-2';
   
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
   
-  interface Dataset {
-    label: string;
-    data: number[];
-    backgroundColor: string;
-    borderColor: string;
-    borderWidth: number;
-  }
+//   ChartJS.register(
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+//   );
   
-  interface HistogramChartProps {
-    labels: string[];
-    datasets: Dataset[];
-    title?: string;
-    yAxisMax?: number;
-  }
+//   interface Dataset {
+//     label: string;
+//     data: number[];
+//     backgroundColor: string;
+//     borderColor: string;
+//     borderWidth: number;
+//   }
   
-  export default function HistogramChart({
-    labels,
-    datasets,
-    title,
-    yAxisMax = 100
-  }: HistogramChartProps) {
-    const options = {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top' as const
-        },
-        title: {
-          display: !!title,
-          text: title
-        }
-      },
-      scales: {
-        y: {
-          beginAtZero: true,
-          max: yAxisMax,
-          ticks: {
-            stepSize: 20
-          }
-        }
-      },
-      barPercentage: 0.9,
-      categoryPercentage: 0.9
-    };
+//   interface HistogramChartProps {
+//     labels: string[];
+//     datasets: Dataset[];
+//     title?: string;
+//     yAxisMax?: number;
+//   }
   
-    const data = {
-      labels,
-      datasets
-    };
+//   export default function HistogramChart({
+//     labels,
+//     datasets,
+//     title,
+//     yAxisMax = 100
+//   }: HistogramChartProps) {
+//     const options = {
+//       responsive: true,
+//       plugins: {
+//         legend: {
+//           position: 'top' as const
+//         },
+//         title: {
+//           display: !!title,
+//           text: title
+//         }
+//       },
+//       scales: {
+//         y: {
+//           beginAtZero: true,
+//           max: yAxisMax,
+//           ticks: {
+//             stepSize: 20
+//           }
+//         }
+//       },
+//       barPercentage: 0.9,
+//       categoryPercentage: 0.9
+//     };
   
-    return <Bar options={options} data={data} />;
-  }
+//     const data = {
+//       labels,
+//       datasets
+//     };
+  
+//     return <Bar options={options} data={data} />;
+//   }
   
