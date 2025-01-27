eb550980566cd4bd90c9c0c3d11cf293bb675b73
Heather Davies <heather@Heathers-MacBook-Pro-2.local>
2025-01-26 22:13:27 -0500

    asdf

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -15,7 +15,6 @@ const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <TooltipPrimitive.Portal>
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
