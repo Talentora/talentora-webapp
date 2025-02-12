21e7eded375e26d5c93ab514d2809fba14cc2865
AvdeepKaur <avkaur@bu.edu>
2025-02-12 18:52:02 -0500

    public pages should all use dynamic language, making it easier to change just from main.css

--- app/(pages)/(public)/signin/page.tsx
+++ app/(pages)/(public)/signin/page.tsx
@@ -25,10 +25,8 @@ export default function SignIn() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-4xl font-bold mb-4 text-foreground">Sign In</h1>
-      <h1 className="text-3xl font-bold mb-8 text-foreground">
-        Choose Your Role
-      </h1>
+      <h1 className="text-4xl font-bold mb-4">Sign In</h1>
+      <h1 className="text-3xl font-bold mb-8 ">Choose Your Role</h1>
       <div className="flex flex-col md:flex-row justify-center gap-6 w-full max-w-3xl">
         <div
           className={`flex-1 flex flex-col items-center justify-center p-8 rounded-lg bg-background border border-input shadow-md cursor-pointer transition-all duration-300 hover:shadow-lg ${
@@ -39,7 +37,7 @@ export default function SignIn() {
           onClick={() => handleUserTypeSelection('recruiter')}
         >
           <h2 className="text-2xl font-semibold mb-2">Recruiter</h2>
-          <p className="text-center text-gray-600">
+          <p className="text-center text-muted-foreground">
             I'm looking to hire talent
           </p>
           <span className="text-sm text-primary">Sign in with SSO</span>
@@ -53,7 +51,7 @@ export default function SignIn() {
           onClick={() => handleUserTypeSelection('applicant')}
         >
           <h2 className="text-2xl font-semibold mb-2">Applicant</h2>
-          <p className="text-center text-gray-600">
+          <p className="text-center text-muted-foreground">
             I'm looking for job opportunities
           </p>
         </div>
