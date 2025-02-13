916d7789875baa330556c3632de40ce63177a91b
Lucas <junsuny789@gmail.com>
2025-02-13 15:41:49 -0500

    pull2

--- components/Layout/Sidebar/DynamicSidebar.tsx
+++ components/Layout/Sidebar/DynamicSidebar.tsx
@@ -26,7 +26,7 @@ export default function DynamicSidebar() {
         fetchRole();
     }, []);
 
-    if (!isRecruiter) return null;
+    // if (!isRecruiter) return null;
     return (
         <aside className="fixed top-0 left-0 h-full w-64 min-w-[16rem] max-w-[20rem] z-[100]">
             <Sidebar />
