cb1a3d635d2d9f69002fcf2d5688ad5fb13dc1dc
Lucas <junsuny789@gmail.com>
2025-02-12 20:36:35 -0500

    asdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,14 @@ export const metadata: Metadata = {
   title: title,
   description: description
 };
+
 export default async function RootLayout({ children }: PropsWithChildren) {
   const supabase = createClient();
   const {
     data: { user }
   } = await supabase.auth.getUser();
+
+
 
   let isSidebarVisible = false;
 
@@ -36,23 +39,35 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   } else {
     isSidebarVisible = false;
   }
-  console.log(user, "user in layout.tsx");
-  console.log("isSidebarVisible??", isSidebarVisible);
+  console.log(user, "user in layout.tsx")
+  console.log("issidebarvisible??", isSidebarVisible);
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen w-full bg-gradient-to-br from-purple-500/[0.1] via-background to-pink-500/[0.1] p-0">
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
           <NextTopLoader />
           <ReactQueryProvider>
             <div className="flex min-h-screen">
-              {isSidebarVisible && <DynamicSidebar/>}
-              <main id="skip" className="flex-1 min-h-screen">
+         
+               {/* Render the dynamic sidebar */}
+              <DynamicSidebar/>
+              <main
+                id="skip"
+                className={`flex-1 min-h-screen ${
+                  isSidebarVisible ? 'ml-64' : ''
+                }`}
+              >
                 <div className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur">
                   <Navbar visible={isSidebarVisible} />
                   {isSidebarVisible && <BreadcrumbsContainer />}
                 </div>
-                <div>
+                <div >
                   <Suspense fallback={<Loading />}>
                     {children}
                   </Suspense>
