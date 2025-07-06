5039a8c7324800e666c0383746107cca7f1d45ee
MattFromBeijing <mhyan@bu.edu>
2025-07-06 17:21:30 -0400

    i'm at the edge of sanity

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -2,11 +2,9 @@ import { createBrowserClient } from '@supabase/ssr';
 import { Database } from '@/types/types_db';
 
 // Create a single supabase client for the entire application
-export const createClient = () => {
-  console.log('bruh')
+export const createClient = () => 
   createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-}
 
